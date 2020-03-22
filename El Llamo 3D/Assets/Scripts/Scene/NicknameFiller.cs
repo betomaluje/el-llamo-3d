bf932a46a9df877c21b8b31572b4721cba46871f
amaluje <beto.maluje@gmail.com>
2020-03-22 08:37:04 +0100

    ADD random nickname generator

---
+++ El Llamo 3D/Assets/Scripts/Scene/NicknameFiller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NicknameFiller : MonoBehaviour
+{
+    [SerializeField] NicknameGenerator nicknameGenerator;
+    [SerializeField] InputField nicknameText;
+
+    private void OnEnable()
+    {
+        GenerateRandomNickName();
+    }
+
+    public void GenerateRandomNickName()
+    {
+        string nickname = nicknameGenerator.Generate(Random.Range(3, 12));
+        nicknameText.text = nickname;
+    }
+
+}
---
+++ El Llamo 3D/Assets/Scripts/Scene/NicknameGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using UnityEngine;
+
+public class NicknameGenerator : MonoBehaviour
+{
+    char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+    char[] consonants = new char[] { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k',
+    'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z' };
+
+    private System.Random random;
+
+    public string Generate(int length)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        random = new System.Random(System.DateTime.Now.Second);
+
+        //initialize our vowel/consonant flag
+        bool flag = (random.Next(2) == 0);
+        for (int i = 0; i < length; i++)
+        {
+            sb.Append(GetChar(flag));
+            flag = !flag; //invert the vowel/consonant flag
+        }
+        sb[0] = char.ToUpper(sb[0]);
+        return sb.ToString();
+    }
+
+    private char GetChar(bool vowel)
+    {
+        if (vowel)
+        {
+            return vowels[random.Next(vowels.Length)];
+        }
+        return consonants[random.Next(consonants.Length)];
+    }
+}
