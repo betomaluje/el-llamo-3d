210fc0505b4910808345109b4873647bc1f0d154
Beto Maluje <beto.maluje@gmail.com>
2020-05-19 20:51:04 +0200

    ADD healthbar to player UI

--- El Llamo 3D/Assets/Scripts/Player/HealthBar.cs
+++ El Llamo 3D/Assets/Scripts/Player/HealthBar.cs
@@ -1,31 +1,27 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 
 public class HealthBar : MonoBehaviour
 {
+    [SerializeField] private bool alwaysFaceCamera = true;
+
     [SerializeField] private Image foregroundImage;
     [SerializeField] private Image backgroundImage;
 
     [SerializeField] private float updateSpeedSeconds = 0.2f;
-    [SerializeField] private float yAmount = 6f;
-    [SerializeField] private float hideTime = 1f;
-    
-    private bool isShown = false;
-    private Transform mainCameraPos;
-    
-    private void Awake()
-    {
-        GetComponentInParent<Health>().OnHealthChanged = HandleHealth;        
 
-        foregroundImage.DOFade(0, 0);
-        backgroundImage.DOFade(0, 0);
-    }
+    private Transform mainCameraPos;
 
     private void OnEnable()
     {
         mainCameraPos = Camera.main.transform;
+        GetComponentInParent<Health>().OnHealthChanged += HandleHealth;
+    }
+
+    private void OnDisable()
+    {
+        GetComponentInParent<Health>().OnHealthChanged -= HandleHealth;
     }
 
     private void HandleHealth(float healthPercentage)
@@ -35,13 +31,8 @@ public class HealthBar : MonoBehaviour
 
     private IEnumerator ChangePercentage(float healthPercentage)
     {
-        if (!isShown)
-        {
-            Show();
-        }
-
         float prePercentage = foregroundImage.fillAmount;
-        float elapsed = 0f;        
+        float elapsed = 0f;
 
         while (elapsed < updateSpeedSeconds)
         {
@@ -51,16 +42,11 @@ public class HealthBar : MonoBehaviour
         }
 
         foregroundImage.fillAmount = healthPercentage;
-
-        if (healthPercentage <= 0)
-        {
-            Hide();
-        }
     }
 
     private void LateUpdate()
     {
-        if (mainCameraPos == null)
+        if (mainCameraPos == null || !alwaysFaceCamera)
         {
             return;
         }
@@ -68,23 +54,4 @@ public class HealthBar : MonoBehaviour
         transform.rotation = Quaternion.identity;
         transform.LookAt(transform.position + mainCameraPos.forward);
     }
-
-    private void Show()
-    {
-        isShown = true;
-        
-        foregroundImage.DOFade(1, 0.4f);
-        backgroundImage.DOFade(1, 0.4f);       
-    }
-
-    private void Hide()
-    {
-        if (isShown)
-        {           
-            foregroundImage.DOFade(0, hideTime);
-            backgroundImage.DOFade(0, hideTime);
-            isShown = false;
-        }        
-    }
-   
 }
