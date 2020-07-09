59de3c4b3b23dfac0fe8a9bfae9405cafc40d7a1
Beto Maluje <beto.maluje@gmail.com>
2020-07-09 15:08:45 +0200

    UPDATE model using the new Blender Plugin UPDATE fix turret look at script

--- El Llamo 3D/Assets/Scripts/Enemies/Turret/TurretLookAtPlayer.cs
+++ El Llamo 3D/Assets/Scripts/Enemies/Turret/TurretLookAtPlayer.cs
@@ -38,17 +38,9 @@ namespace Llamo.Turret
 
         private void RotateTowards(Vector3 to)
         {
-            Vector3 difference = (to - transform.position).normalized;
-
-            float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-
-            Debug.Log("Z angle " + rotationZ);
-
-            if (rotationZ < 0) rotationZ -= 360;
-
-            var rotationAngle = Quaternion.Euler(-90.0f, 0.0f, rotationZ);
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotationAngle, Time.deltaTime * turnSpeed);
+            Vector3 pos = to - transform.position;
+            Quaternion newRot = Quaternion.LookRotation(pos);
+            transform.rotation = Quaternion.Lerp(transform.rotation, newRot, Time.deltaTime * turnSpeed);
         }
     }
 }
